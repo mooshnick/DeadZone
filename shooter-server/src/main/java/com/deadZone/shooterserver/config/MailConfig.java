30774bcc5ff12a3bcfea99bb4e091691a47963ab
BenayaThomsong <banayatsingsit@gmail.com>
2026-06-25 01:43:46 +0300

    Support SMTP SSL for Render email delivery

--- shooter-server/src/main/java/com/deadZone/shooterserver/config/MailConfig.java
+++ shooter-server/src/main/java/com/deadZone/shooterserver/config/MailConfig.java
@@ -19,10 +19,11 @@ public class MailConfig {
             @Value("${spring.mail.properties.mail.smtp.auth:true}") String auth,
             @Value("${spring.mail.properties.mail.smtp.starttls.enable:true}") String startTls,
             @Value("${spring.mail.properties.mail.smtp.starttls.required:true}") String startTlsRequired,
+            @Value("${spring.mail.properties.mail.smtp.ssl.enable:false}") String sslEnable,
             @Value("${spring.mail.properties.mail.smtp.ssl.trust:smtp.gmail.com}") String sslTrust,
-            @Value("${spring.mail.properties.mail.smtp.connectiontimeout:10000}") String connectionTimeout,
-            @Value("${spring.mail.properties.mail.smtp.timeout:10000}") String timeout,
-            @Value("${spring.mail.properties.mail.smtp.writetimeout:10000}") String writeTimeout
+            @Value("${spring.mail.properties.mail.smtp.connectiontimeout:20000}") String connectionTimeout,
+            @Value("${spring.mail.properties.mail.smtp.timeout:20000}") String timeout,
+            @Value("${spring.mail.properties.mail.smtp.writetimeout:20000}") String writeTimeout
     ) {
         JavaMailSenderImpl sender = new JavaMailSenderImpl();
         sender.setHost(host);
@@ -34,6 +35,7 @@ public class MailConfig {
         properties.put("mail.smtp.auth", auth);
         properties.put("mail.smtp.starttls.enable", startTls);
         properties.put("mail.smtp.starttls.required", startTlsRequired);
+        properties.put("mail.smtp.ssl.enable", sslEnable);
         properties.put("mail.smtp.ssl.trust", sslTrust);
         properties.put("mail.smtp.connectiontimeout", connectionTimeout);
         properties.put("mail.smtp.timeout", timeout);
